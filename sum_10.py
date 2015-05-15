d2ae1d8a57a4beaadacd7697020ceed20998cf5c
Mark Saiget <saiget@gmail.com>
2015-05-14 21:01:25 -0700

    Add additional memory solution.

--- sum_10.py
+++ sum_10.py
@@ -2,7 +2,17 @@
 
 
 def sum10(in_ls):
+    # """double for loop implementations."""
+    # for x in in_ls:
+    #     for y in in_ls:
+    #         if x + y == 10:
+    #             return x, y
+
+    """Additional memory implementation."""
+    foo = []
     for x in in_ls:
-        for y in in_ls:
-            if x + y == 10:
-                return x, y
+        temp = 10-x
+        if temp in foo:
+            return temp, x
+        else:
+            foo.append(temp)
